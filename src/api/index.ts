6e504c9db70c12be1c8964d2cea5d38e3970f6b7
Remco Stoeten <remcostoeten@hotmail.com>
2025-07-04 20:53:32 +0200

    Add todos functionality and setup ORM

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,15 +6,30 @@ import {
   executeQueryHandler, 
   getTablesHandler 
 } from './handlers'
+import {
+  createTodoHandler,
+  getAllTodosHandler,
+  getTodoHandler,
+  updateTodoHandler,
+  deleteTodoHandler
+} from './todoHandlers'
 
 function createApi() {
   const api = new Hono()
   
+  // Database connection routes
   api.get('/connections', getAllConnections)
   api.post('/connections/test', testConnectionHandler)
   api.post('/connections', createConnectionHandler)
   api.post('/connections/:id/query', executeQueryHandler)
   api.get('/connections/:id/tables', getTablesHandler)
+  
+  // Todo routes
+  api.post('/todos', createTodoHandler)
+  api.get('/todos', getAllTodosHandler)
+  api.get('/todos/:id', getTodoHandler)
+  api.put('/todos/:id', updateTodoHandler)
+  api.delete('/todos/:id', deleteTodoHandler)
   
   return api
 }
